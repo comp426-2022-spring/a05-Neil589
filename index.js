09afb317a7aaa12ae2352bb61032870f6158e657
Neil Thupili <69744850+Neil589@users.noreply.github.com>
2022-04-17 09:12:32 -0400

    up till body endpoint changes

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Place your server entry point code here
 
+// Serve static HTML files
+app.use(express.static('./public'));
+
+// Make Express use its own built-in body parser to handle JSON
+app.use(express.json());
 
 
 const express = require('express');
